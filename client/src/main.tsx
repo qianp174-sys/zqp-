f2a463aa163bb312bafcfd84f042292b8548a34c
qianp174-sys <283570002+qianp174-sys@users.noreply.github.com>
2026-05-13 03:37:29 -0400

    Force set document title in JavaScript

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+document.title = "朱倩萍个人网站 | Qianping Zhu";
 
 createRoot(document.getElementById("root")!).render(<App />);
